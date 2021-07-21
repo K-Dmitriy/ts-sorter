9127cbe48b66e1305dd777085e4ef7e7ebbd97b3
d.kozlov <d.kozlov@hottelecom.by>
2021-07-21 12:43:47 +0300

    add class Sorter and method sort (bubble sort)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log(111);
+class Sorter {
+	constructor(public collection: number[]) { }
+
+	sort(): void {
+		const { length } = this.collection;
+
+		for (let j = 0; j < length; j++) {
+			for (let i = 0; i < length - j - 1; i++) {
+				
+				const currentElement = this.collection[i];
+
+				if (i !== length && currentElement > this.collection[i + 1]) {
+					this.collection[i] = this.collection[i + 1];
+					this.collection[i + 1] = currentElement;
+				}
+			}
+		}
+	}
+}
+
+const sorter = new Sorter([3, -4, 29, 5, 2, 11]);
+sorter.sort();
+
+console.log(sorter);
